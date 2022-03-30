ab2bc613cd26b67fe6ea24b7c3d494d5ea329b18
begbert@gmail.com <begbert95@gmail.com>
2022-03-29 18:02:37 -0600

    started initial setup

---
+++ inventory.java
@@ -0,0 +1,21 @@
+import java.util.Observable;
+
+public class inventory {
+    private static allParts;
+    private static allProducts;
+
+    public static void addPart(newPart: Part) {
+        allParts.add(newPart);
+    }
+
+    public static void deletePart (selectedPart:Part){
+        allParts.remove(selectedPart);
+    };
+
+    public static void updatePart;
+    public static void addProduct;
+    public static void deleteProduct;
+    public static void updateProduct;
+    public static getAllParts;
+    public static getAllProducts;
+}
